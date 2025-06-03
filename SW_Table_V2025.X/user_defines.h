b3e5243cf67986f985c8b6116eb6997b77328ee2
Migács Máté <matemigacs@gmail.com>
2025-06-03 14:54:43 +0200

    user_define.h ifdef corrected

--- SW_Table_V2025.X/user_defines.h
+++ SW_Table_V2025.X/user_defines.h
@@ -28,8 +28,8 @@
 
 // This is a guard condition so that contents of this file are not included
 // more than once.  
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
+#ifndef USER_DEFINE_H
+#define	USER_DEFINE_H
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 
@@ -74,5 +74,5 @@ extern "C" {
 }
 #endif /* __cplusplus */
 
-#endif	/* XC_HEADER_TEMPLATE_H */
+#endif	/*USER_DEFINE_H */
 
